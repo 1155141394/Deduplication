b4bf3a68f31aa01bf8445d3fbb0eba9d9df70a03
TaoXU0 <464612404@qq.com>
2022-12-12 15:53:11 +0800

    Add metrics print

--- MyDedup.java
+++ MyDedup.java
@@ -105,11 +105,17 @@ public class MyDedup {
             ArrayList<String> fileRecipe = new ArrayList<>(); // File recipe
             int len = 0;
             String basicPath = "data/"; // Need to change
-
+            int numOfFile = 1;
+            int numPreDedChunk = chunks.size();
+            int numUniqueChunk = 0;
+            int bytePreDedChunk = 0;
+            int byteUniqueChunk = 0;
             // Loop the chunks
             for (ArrayList<Byte> byteList: chunks)
             {
-                //System.out.println(byteList);
+                // Calculation
+                bytePreDedChunk += byteList.size();
+                // System.out.println(byteList);
                 byte[] byteArr = byteList2Arr(byteList);
                 len = byteList.size();
                 // Check if the container has enough space
@@ -125,6 +131,8 @@ public class MyDedup {
                 byte[] checksum = getChecksum(byteArr);
                 String fingerprint = Arrays.toString(checksum);
                 indexes.add(fingerprint + ";" + containerNum + ";" + offset + ";" + len);
+                byteUniqueChunk += byteList.size();
+                numUniqueChunk += 1;
                 // Store the fingerprint in file recipe
                 fileRecipe.add(fingerprint);
                 // Add chunk to buffer
@@ -145,19 +153,39 @@ public class MyDedup {
             // Store file recipe
             String fileRecipePath = basicPath + uploadFileName + ".txt";
             strList2File(fileRecipePath, fileRecipe);
-            // Store the container number
-            String conNumPath = basicPath + "containNum.txt";
+            // Store the container number, total num of files, num of prechunk, num of unique chunk,
+            // bytes of pre chunk, bytes of unique chunk
+            String conNumPath = basicPath + "info.txt";
             ArrayList<String> tmp = new ArrayList<>();
             tmp.add(Integer.toString(containerNum));
+            tmp.add(Integer.toString(numOfFile));
+            tmp.add(Integer.toString(numPreDedChunk));
+            tmp.add(Integer.toString(numUniqueChunk));
+            tmp.add(Integer.toString(bytePreDedChunk));
+            tmp.add(Integer.toString(byteUniqueChunk));
             strList2File(conNumPath, tmp);
+            System.out.println("Report Output:");
+            System.out.printf("Total number of files that have been stored: %d\n", numOfFile);
+            System.out.printf("Total number of pre-deduplicated chunks in storage: %d\n", numPreDedChunk);
+            System.out.printf("Total number of unique chunks in storage: %d\n", numUniqueChunk);
+            System.out.printf("Total number of bytes of pre-deduplicated chunks in storage: %d\n", bytePreDedChunk);
+            System.out.printf("Total number of bytes of unique chunks in storage: %d\n", byteUniqueChunk);
+            System.out.printf("Total number of containers in storage: %d\n", containerNum-1);
+            double deduRatio = (double) bytePreDedChunk/byteUniqueChunk;
+            System.out.printf("Deduplication ratio: %.2f\n", deduRatio);
         }
         // Exist the index file
         else
         {
             ArrayList<Byte> container = new ArrayList<>(); // New container
             // Get the container number from the file
-            ArrayList<String> conNumList = file2StrList("data/containNum.txt");
+            ArrayList<String> conNumList = file2StrList("data/info.txt");
             int containerNum = Integer.parseInt(conNumList.get(0));
+            int numOfFile = Integer.parseInt(conNumList.get(1)) + 1;
+            int numPreDedChunk = Integer.parseInt(conNumList.get(2)) + chunks.size();
+            int numUniqueChunk = Integer.parseInt(conNumList.get(3));
+            int bytePreDedChunk = Integer.parseInt(conNumList.get(4));
+            int byteUniqueChunk = Integer.parseInt(conNumList.get(5));
             // Get index and store in a map
             ArrayList<String> indexes = file2StrList("data/mydedup.index");
             HashMap<String, Integer> indexMap = new HashMap<>();
@@ -176,6 +204,7 @@ public class MyDedup {
             {
                 byte[] byteArr = byteList2Arr(byteList);
                 len = byteList.size();
+                bytePreDedChunk += len;
                 // Check if the container has enough space
                 if (offset + len >= maxContainerSize) {
                     // Store container
@@ -194,6 +223,8 @@ public class MyDedup {
                     indexes.add(fingerprint + ";" + containerNum + ";" + offset + ";" + len);
                     container.addAll(byteList);
                     offset += len;
+                    numUniqueChunk += 1;
+                    byteUniqueChunk += byteList.size();
                 }
 //                else System.out.println(byteList.size());
                 fileRecipe.add(fingerprint);
@@ -213,11 +244,25 @@ public class MyDedup {
             // Store file recipe
             String fileRecipePath = basicPath + uploadFileName + ".txt";
             strList2File(fileRecipePath, fileRecipe);
-            // Store the container number
-            String conNumPath = basicPath + "containNum.txt";
+            // Store the info
+            String conNumPath = basicPath + "info.txt";
             ArrayList<String> tmp = new ArrayList<>();
             tmp.add(Integer.toString(containerNum));
+            tmp.add(Integer.toString(numOfFile));
+            tmp.add(Integer.toString(numPreDedChunk));
+            tmp.add(Integer.toString(numUniqueChunk));
+            tmp.add(Integer.toString(bytePreDedChunk));
+            tmp.add(Integer.toString(byteUniqueChunk));
             strList2File(conNumPath, tmp);
+            System.out.println("Report Output:");
+            System.out.printf("Total number of files that have been stored: %d\n", numOfFile);
+            System.out.printf("Total number of pre-deduplicated chunks in storage: %d\n", numPreDedChunk);
+            System.out.printf("Total number of unique chunks in storage: %d\n", numUniqueChunk);
+            System.out.printf("Total number of bytes of pre-deduplicated chunks in storage: %d\n", bytePreDedChunk);
+            System.out.printf("Total number of bytes of unique chunks in storage: %d\n", byteUniqueChunk);
+            System.out.printf("Total number of containers in storage: %d\n", containerNum-1);
+            double deduRatio = (double) bytePreDedChunk/byteUniqueChunk;
+            System.out.printf("Deduplication ratio: %.2f\n", deduRatio);
         }
         return 0;
     }
@@ -279,7 +324,7 @@ public class MyDedup {
         InputStream in = null;
         byte[] buffer;
         try {
-            System.out.println("以字节为单位读取文件内容，一次读一个字节：");
+//            System.out.println("以字节为单位读取文件内容，一次读一个字节：");
             // 一次读一个字节
             in = new FileInputStream(file);
             buffer = file2byte(fileName);
@@ -289,7 +334,7 @@ public class MyDedup {
             return null;
         }
         int size = buffer.length;
-        System.out.println(size);
+//        System.out.println(size);
 
 
         ArrayList<ArrayList<Byte>> chunks = new ArrayList<>();
@@ -345,7 +390,7 @@ public class MyDedup {
 
 
             ArrayList<ArrayList<Byte>> chunks = getChunks(fileName,m,d,q,maxSize);
-            System.out.println("Chunks size is "+chunks.size());
+//            System.out.println("Chunks size is "+chunks.size());
             try {
                 chunkUpload(chunks, "mydedup.index", fileName);
             }
